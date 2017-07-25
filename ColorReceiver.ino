350a21f21a64c4582f1b648f8db1fa681b1bd0cc
Logan Saso <logan.saso2016@gmail.com>
2017-07-24 22:05:55 -0700

    Create ColorReceiver.ino

---
+++ ColorReceiver.ino
@@ -0,0 +1,79 @@
+#include <NeoPixelBus.h>
+#include <ArduinoJson.h>
+
+
+const uint16_t PixelCount = 120; // this example assumes 4 pixels, making it smaller will cause a failure
+const uint8_t PixelPin = 3;  // make sure to set this to the correct pin, ignored for Esp8266
+
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
+
+
+#include <SPI.h>         // needed for Arduino versions later than 0018
+#include <Ethernet.h>
+#include <EthernetUdp.h>         // UDP library from: bjoern@cs.stanford.edu 12/30/2008
+
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEC
+};
+IPAddress ip(192, 168, 1, 177);
+
+unsigned int localPort = 11135;      // local port to listen on
+
+// buffers for receiving and sending data
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];  //buffer to hold incoming packet,
+char  ReplyBuffer[] = "acknowledged";       // a string to send back
+
+// An EthernetUDP instance to let us send and receive packets over UDP
+EthernetUDP Udp;
+
+void setup() {
+  // start the Ethernet and UDP:
+  Ethernet.begin(mac, ip);
+  Udp.begin(localPort);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // if there's data available, read a packet
+  int packetSize = Udp.parsePacket();
+  if (packetSize) {
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+    Serial.print("From ");
+    IPAddress remote = Udp.remoteIP();
+    for (int i = 0; i < 4; i++) {
+      Serial.print(remote[i], DEC);
+      if (i < 3) {
+        Serial.print(".");
+      }
+    }
+    Serial.print(", port ");
+    Serial.println(Udp.remotePort());
+
+    // read the packet into packetBufffer
+    Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    Serial.print(packetBuffer);
+    color(packetBuffer);
+
+    // send a reply to the IP address and port that sent us the packet we received
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(ReplyBuffer);
+    Udp.endPacket();
+  }
+}
+
+void color(char packetBuffer[]){
+  DynamicJsonBuffer jsonBuffer;
+  JsonArray& colorArray = jsonBuffer.parseArray(packetBuffer);
+  JsonObject& object = colorArray[0];
+  for(int i = 1; i < sizeof(colorArray) / sizeof(colorArray[0]); i++){
+    Serial.print("Array Location" + i);
+    strip.SetPixelColor(object["index"], RgbColor(object["data"]["red"], object["data"]["green"], object["data"]["blue"]));
+  }
+  
+}
+
